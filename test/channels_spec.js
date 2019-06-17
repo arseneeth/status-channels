f587ac11cbbc00481b3f90018feef8645afb2dfb
0x332 <kinspotting@protonmail.com>
2019-06-17 12:22:55 +0800

    add: multiple accounts

--- test/channels_spec.js
+++ test/channels_spec.js
@@ -4,10 +4,14 @@
 
 // basic StatusJS instantiation
 const StatusJS = require('status-js-api');
-const status = new StatusJS
+const status_1 = new StatusJS
+const status_2 = new StatusJS
 
-const privKey = "0x7847A6D27AAD97D4C6FDC93F47CCD386F3A9DA8065EAF1A64DB5A284FE6BA76D";
+const privKey_1 = "0x7847A6D27AAD97D4C6FDC93F47CCD386F3A9DA8065EAF1A64DB5A284FE6BA76D"; // TODO manage private key issue later
+const privKey_2 = "0x11BA3F03E99672DAE1E23661BE19021D3EA69C7F43FFA1B58D742E5FD3553321"; 
 
+				// NOTE: it's a test private key, for test purpose only
+ 
 // contracts
 
 const Signatures = require('Embark/contracts/Signatures');
@@ -15,7 +19,7 @@ const Signatures = require('Embark/contracts/Signatures');
 config({
   contracts: {
      Signatures: {
-      // args: ["$License", "$Arbitration"]
+     	address: '0x92cdB4b878aC7DA5897a2c55c4E54FE653A89416' // NOTE: comment this section if you want to deploy a new contract
     }
   }
 }, (_err, web3_accounts) => {
@@ -29,30 +33,30 @@ contract("Channels", function () {
 	  before(async () => {
 
 	    let hash = await Signatures.methods.getDataHash("Iuri", [0], "London").call();
-//	    signature = await web3.eth.sign(hash, accounts[0]);
-
-		await console.log(hash);
+	    let signature = await web3.eth.sign(hash, accounts[0]);
 
 	  });
 
 
-	it("it should connect to ethereum node", async function () {
+	it("it should establish connection for Status client 1", async function () {
 
-	  	await status.connect("ws://localhost:8546", privKey);
-	  	// await status.connect("ws://localhost:8546", privateKey);
-	  	await console.log("connected");
+	  	await status_1.connect("ws://localhost:8546", privKey_1);
+	  	// await console.log("Status client 1 connected");
 
-	  	// try{
-	  	// 	await status.joinChat("#mytest");
-	  	// } catch (error){
-	  	// 	await console.log(error);
-	  	// }
+	});
+
+
+	it("it should establish connection for Status client 2", async function () {
+
+	  	await status_2.connect("ws://localhost:8546", privKey_2);
+	  	// await console.log("Status client 2 connected");
+	});
 
 	  	// TODO:
 	  	//1* set up connection between two nodes
 	  	//2* generate signature on one node
 	  	//3* pass signature to another node
-	});
+
 
 })
 ;
